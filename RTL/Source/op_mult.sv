7eaf1cf355825528f97f49181566ba6fb86fed02
Kolbenkov Pavel <kolbenkov.pavel@gmail.com>
2024-03-22 09:47:20 +0300

    Optimized code of arithmetic blocks (multiplication and accumulation) shortened critical paths

--- RTL/Source/op_mult.sv
+++ RTL/Source/op_mult.sv
@@ -9,15 +9,11 @@ module multa2
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [38:0] Out1;  // sfix39_En29
 
-  wire signed [50:0] multa2_mul_temp;  // sfix51_En38
-  wire signed [38:0] multa2_out1;  // sfix39_En29
+  wire signed [47:0] multa2_mul_temp;  // sfix48_En38
 
   assign multa2_mul_temp = In1 * In2;
-  assign multa2_out1 = (((multa2_mul_temp[50] == 1'b0) && (multa2_mul_temp[49:47] != 3'b000)) || ((multa2_mul_temp[50] == 1'b0) && (multa2_mul_temp[47:9] == 39'sh3FFFFFFFFF)) ? 39'sh3FFFFFFFFF :
-              ((multa2_mul_temp[50] == 1'b1) && (multa2_mul_temp[49:47] != 3'b111) ? 39'sh4000000000 :
-              multa2_mul_temp[47:9] + $signed({1'b0, multa2_mul_temp[8]})));
 
-  assign Out1 = multa2_out1;
+  assign Out1 = multa2_mul_temp[47:9] + multa2_mul_temp[8];
 
 endmodule  // multa2
 
@@ -30,15 +26,11 @@ module multa4
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [43:0] Out1;  // sfix44_En33
 
-  wire signed [50:0] multa4_mul_temp;  // sfix51_En38
-  wire signed [43:0] multa4_out1;  // sfix44_En33
+  wire signed [48:0] multa4_mul_temp;  // sfix49_En38
 
   assign multa4_mul_temp = In1 * In2;
-  assign multa4_out1 = (((multa4_mul_temp[50] == 1'b0) && (multa4_mul_temp[49:48] != 2'b00)) || ((multa4_mul_temp[50] == 1'b0) && (multa4_mul_temp[48:5] == 44'sh7FFFFFFFFFF)) ? 44'sh7FFFFFFFFFF :
-              ((multa4_mul_temp[50] == 1'b1) && (multa4_mul_temp[49:48] != 2'b11) ? 44'sh80000000000 :
-              multa4_mul_temp[48:5] + $signed({1'b0, multa4_mul_temp[4]})));
 
-  assign Out1 = multa4_out1;
+  assign Out1 = multa4_mul_temp[48:5] + multa4_mul_temp[4];
 
 endmodule  // multa4
 
@@ -51,15 +43,11 @@ module multa6
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [28:0] Out1;  // sfix29_En18
 
-  wire signed [50:0] multa6_mul_temp;  // sfix51_En38
-  wire signed [28:0] multa6_out1;  // sfix29_En18
+  wire signed [48:0] multa6_mul_temp;  // sfix49_En38
 
   assign multa6_mul_temp = In1 * In2;
-  assign multa6_out1 = (((multa6_mul_temp[50] == 1'b0) && (multa6_mul_temp[49:48] != 2'b00)) || ((multa6_mul_temp[50] == 1'b0) && (multa6_mul_temp[48:20] == 29'sb01111111111111111111111111111)) ? 29'sb01111111111111111111111111111 :
-              ((multa6_mul_temp[50] == 1'b1) && (multa6_mul_temp[49:48] != 2'b11) ? 29'sb10000000000000000000000000000 :
-              multa6_mul_temp[48:20] + $signed({1'b0, multa6_mul_temp[19]})));
 
-  assign Out1 = multa6_out1;
+  assign Out1 = multa6_mul_temp[48:20] + multa6_mul_temp[19];
 
 endmodule  // multa6
 
@@ -72,15 +60,11 @@ module multa8
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [35:0] Out1;  // sfix36_En26
 
-  wire signed [50:0] multa8_mul_temp;  // sfix51_En38
-  wire signed [35:0] multa8_out1;  // sfix36_En26
+  wire signed [47:0] multa8_mul_temp;  // sfix48_En38
 
   assign multa8_mul_temp = In1 * In2;
-  assign multa8_out1 = (((multa8_mul_temp[50] == 1'b0) && (multa8_mul_temp[49:47] != 3'b000)) || ((multa8_mul_temp[50] == 1'b0) && (multa8_mul_temp[47:12] == 36'sh7FFFFFFFF)) ? 36'sh7FFFFFFFF :
-              ((multa8_mul_temp[50] == 1'b1) && (multa8_mul_temp[49:47] != 3'b111) ? 36'sh800000000 :
-              multa8_mul_temp[47:12] + $signed({1'b0, multa8_mul_temp[11]})));
 
-  assign Out1 = multa8_out1;
+  assign Out1 = multa8_mul_temp[47:12] + multa8_mul_temp[11];
 
 endmodule  // multa8
 
@@ -93,15 +77,11 @@ module multa10
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [25:0] Out1;  // sfix26_En17
 
-  wire signed [50:0] multa10_mul_temp;  // sfix51_En38
-  wire signed [25:0] multa10_out1;  // sfix26_En17
+  wire signed [46:0] multa10_mul_temp;  // sfix47_En38
 
   assign multa10_mul_temp = In1 * In2;
-  assign multa10_out1 = (((multa10_mul_temp[50] == 1'b0) && (multa10_mul_temp[49:46] != 4'b0000)) || ((multa10_mul_temp[50] == 1'b0) && (multa10_mul_temp[46:21] == 26'sb01111111111111111111111111)) ? 26'sb01111111111111111111111111 :
-              ((multa10_mul_temp[50] == 1'b1) && (multa10_mul_temp[49:46] != 4'b1111) ? 26'sb10000000000000000000000000 :
-              multa10_mul_temp[46:21] + $signed({1'b0, multa10_mul_temp[20]})));
 
-  assign Out1 = multa10_out1;
+  assign Out1 = multa10_mul_temp[46:21] + multa10_mul_temp[20];
 
 endmodule  // multa10
 
@@ -114,15 +94,11 @@ module multa12
   input   signed [35:0] In1;  // sfix36_En27
   output  signed [26:0] Out1;  // sfix27_En19
 
-  wire signed [50:0] multa12_mul_temp;  // sfix51_En38
-  wire signed [26:0] multa12_out1;  // sfix27_En19
+  wire signed [45:0] multa12_mul_temp;  // sfix46_En38
 
   assign multa12_mul_temp = In1 * In2;
-  assign multa12_out1 = (((multa12_mul_temp[50] == 1'b0) && (multa12_mul_temp[49:45] != 5'b00000)) || ((multa12_mul_temp[50] == 1'b0) && (multa12_mul_temp[45:19] == 27'sb011111111111111111111111111)) ? 27'sb011111111111111111111111111 :
-              ((multa12_mul_temp[50] == 1'b1) && (multa12_mul_temp[49:45] != 5'b11111) ? 27'sb100000000000000000000000000 :
-              multa12_mul_temp[45:19] + $signed({1'b0, multa12_mul_temp[18]})));
 
-  assign Out1 = multa12_out1;
+  assign Out1 = multa12_mul_temp[45:19] + multa12_mul_temp[18];
 
 endmodule  // multa12
 
@@ -136,14 +112,10 @@ module multb0
   output  signed [21:0] Out1;  // sfix22_En14
 
   wire signed [22:0] multb0_mul_temp;  // sfix23_En14
-  wire signed [21:0] multb0_out1;  // sfix22_En14
 
   assign multb0_mul_temp = In1 * In2;
-  assign multb0_out1 = ((multb0_mul_temp[22] == 1'b0) && (multb0_mul_temp[21] != 1'b0) ? 22'sb0111111111111111111111 :
-              ((multb0_mul_temp[22] == 1'b1) && (multb0_mul_temp[21] != 1'b1) ? 22'sb1000000000000000000000 :
-              $signed(multb0_mul_temp[21:0])));
 
-  assign Out1 = multb0_out1;
+  assign Out1 = multb0_mul_temp[21:0];
 
 endmodule  // multb0_1
 
@@ -157,14 +129,10 @@ module multb2
   output  signed [20:0] Out1;  // sfix21_En13
 
   wire signed [22:0] multb2_mul_temp;  // sfix23_En14
-  wire signed [20:0] multb2_out1;  // sfix21_En13
 
   assign multb2_mul_temp = In1 * In2;
-  assign multb2_out1 = (((multb2_mul_temp[22] == 1'b0) && (multb2_mul_temp[21] != 1'b0)) || ((multb2_mul_temp[22] == 1'b0) && (multb2_mul_temp[21:1] == 21'sb011111111111111111111)) ? 21'sb011111111111111111111 :
-              ((multb2_mul_temp[22] == 1'b1) && (multb2_mul_temp[21] != 1'b1) ? 21'sb100000000000000000000 :
-              multb2_mul_temp[21:1] + $signed({1'b0, multb2_mul_temp[0]})));
 
-  assign Out1 = multb2_out1;
+  assign Out1 = multb2_mul_temp[21:1] + multb2_mul_temp[0];
 
 endmodule  // multb2_1
 
@@ -178,14 +146,10 @@ module multb4
   output  signed [20:0] Out1;  // sfix21_En13
 
   wire signed [22:0] multb4_mul_temp;  // sfix23_En14
-  wire signed [20:0] multb4_out1;  // sfix21_En13
 
   assign multb4_mul_temp = In1 * In2;
-  assign multb4_out1 = (((multb4_mul_temp[22] == 1'b0) && (multb4_mul_temp[21] != 1'b0)) || ((multb4_mul_temp[22] == 1'b0) && (multb4_mul_temp[21:1] == 21'sb011111111111111111111)) ? 21'sb011111111111111111111 :
-              ((multb4_mul_temp[22] == 1'b1) && (multb4_mul_temp[21] != 1'b1) ? 21'sb100000000000000000000 :
-              multb4_mul_temp[21:1] + $signed({1'b0, multb4_mul_temp[0]})));
 
-  assign Out1 = multb4_out1;
+  assign Out1 = multb4_mul_temp[21:1] + multb4_mul_temp[0];
 
 endmodule  // multb4_1
 
@@ -199,13 +163,9 @@ module multb6
   output  signed [19:0] Out1;  // sfix20_En12
 
   wire signed [22:0] multb6_mul_temp;  // sfix23_En14
-  wire signed [19:0] multb6_out1;  // sfix20_En12
 
   assign multb6_mul_temp = In1 * In2;
-  assign multb6_out1 = (((multb6_mul_temp[22] == 1'b0) && (multb6_mul_temp[21] != 1'b0)) || ((multb6_mul_temp[22] == 1'b0) && (multb6_mul_temp[21:2] == 20'sb01111111111111111111)) ? 20'sb01111111111111111111 :
-              ((multb6_mul_temp[22] == 1'b1) && (multb6_mul_temp[21] != 1'b1) ? 20'sb10000000000000000000 :
-              multb6_mul_temp[21:2] + $signed({1'b0, multb6_mul_temp[1]})));
 
-  assign Out1 = multb6_out1;
+  assign Out1 = multb6_mul_temp[21:2] + multb6_mul_temp[1];
 
 endmodule  // multb6
--- RTL/Source/op_sum.sv
+++ RTL/Source/op_sum.sv
@@ -9,25 +9,13 @@ module sumin
   input   signed [38:0] In2;  // sfix39_En28
   output  signed [35:0] Out1;  // sfix36_En27
 
-  wire signed [35:0] sumin_add_cast;  // sfix36_En27
-  wire signed [36:0] sumin_add_cast_1;  // sfix37_En27
-  wire signed [35:0] sumin_add_cast_2;  // sfix36_En27
-  wire signed [36:0] sumin_add_cast_3;  // sfix37_En27
-  wire signed [36:0] sumin_add_temp;  // sfix37_En27
-  wire signed [35:0] sumin_out1;  // sfix36_En27
+  wire signed [35:0] sumin_add_cast1;  // sfix36_En27
+  wire signed [35:0] sumin_add_cast2;  // sfix36_En27
 
-  assign sumin_add_cast = {In1[21], {In1, 13'b0000000000000}};
-  assign sumin_add_cast_1 = {sumin_add_cast[35], sumin_add_cast};
-  assign sumin_add_cast_2 = (((In2[38] == 1'b0) && (In2[37:36] != 2'b00)) || ((In2[38] == 1'b0) && (In2[36:1] == 36'sh7FFFFFFFF)) ? 36'sh7FFFFFFFF :
-              ((In2[38] == 1'b1) && (In2[37:36] != 2'b11) ? 36'sh800000000 :
-              In2[36:1] + $signed({1'b0, In2[0]})));
-  assign sumin_add_cast_3 = {sumin_add_cast_2[35], sumin_add_cast_2};
-  assign sumin_add_temp = sumin_add_cast_1 + sumin_add_cast_3;
-  assign sumin_out1 = ((sumin_add_temp[36] == 1'b0) && (sumin_add_temp[35] != 1'b0) ? 36'sh7FFFFFFFF :
-              ((sumin_add_temp[36] == 1'b1) && (sumin_add_temp[35] != 1'b1) ? 36'sh800000000 :
-              $signed(sumin_add_temp[35:0])));
+  assign sumin_add_cast1 = {In1[21], In1, 13'b0};
+  assign sumin_add_cast2 = In2[36:1] + In2[0];
 
-  assign Out1 = sumin_out1;
+  assign Out1 = sumin_add_cast1 + sumin_add_cast2;
 
 endmodule  // sumin
 
@@ -40,22 +28,12 @@ module sumq
   input   signed [41:0] In1;  // sfix42_En31
   output  signed [38:0] Out1;  // sfix39_En28
 
-  wire signed [38:0] sumq_1_add_cast;  // sfix39_En28
-  wire signed [39:0] sumq_1_add_cast_1;  // sfix40_En28
-  wire signed [39:0] sumq_1_add_cast_2;  // sfix40_En28
-  wire signed [39:0] sumq_1_add_temp;  // sfix40_En28
-  wire signed [38:0] sumq_1_out1;  // sfix39_En28
+  wire signed [38:0] sumq_1_add_cast1;  // sfix39_En28
+  wire signed [38:0] sumq_1_add_cast2;  // sfix39_En28
 
-  assign sumq_1_add_cast = ((In1[41] == 1'b0) && (In1[40:3] == 38'h3FFFFFFFFF) ? 39'sh3FFFFFFFFF :
-              In1[41:3] + $signed({1'b0, In1[2]}));
-  assign sumq_1_add_cast_1 = {sumq_1_add_cast[38], sumq_1_add_cast};
-  assign sumq_1_add_cast_2 = {In2[33], {In2, 5'b00000}};
-  assign sumq_1_add_temp = sumq_1_add_cast_1 + sumq_1_add_cast_2;
-  assign sumq_1_out1 = ((sumq_1_add_temp[39] == 1'b0) && (sumq_1_add_temp[38] != 1'b0) ? 39'sh3FFFFFFFFF :
-              ((sumq_1_add_temp[39] == 1'b1) && (sumq_1_add_temp[38] != 1'b1) ? 39'sh4000000000 :
-              $signed(sumq_1_add_temp[38:0])));
-
-  assign Out1 = sumq_1_out1;
+  assign sumq_1_add_cast1 = In1[41:3] + In1[2];
+  assign sumq_1_add_cast2 = {In2, 5'b00000};
+  assign Out1 = sumq_1_add_cast1 + sumq_1_add_cast2;
 
 endmodule  // sumq
 
@@ -68,17 +46,7 @@ module sumd
   input   signed [27:0] In2;  // sfix28_En17
   output  signed [33:0] Out1;  // sfix34_En23
 
-  wire signed [33:0] sumd_2_add_cast;  // sfix34_En22
-  wire signed [33:0] sumd_2_add_cast_1;  // sfix34_En22
-  wire signed [33:0] sumd_2_add_temp;  // sfix34_En22
-  wire signed [33:0] sumd_2_out1;  // sfix34_En23
-
-  assign sumd_2_add_cast = {In1[32], In1};
-  assign sumd_2_add_cast_1 = {In2[27], {In2, 5'b00000}};
-  assign sumd_2_add_temp = sumd_2_add_cast + sumd_2_add_cast_1;
-  assign sumd_2_out1 = {sumd_2_add_temp[32:0], 1'b0};
-
-  assign Out1 = sumd_2_out1;
+  assign Out1 = {In1 + {In2, 5'b00000}, 1'b0};
 
 endmodule  // sumd
 
@@ -91,15 +59,12 @@ module suma2
   input   signed [35:0] In1;  // sfix36_En26
   output  signed [32:0] Out1;  // sfix33_En22
 
-  wire signed [32:0] suma2_add_cast;  // sfix33_En22
-  wire signed [32:0] suma2_add_cast_1;  // sfix33_En22
-  wire signed [32:0] suma2_out1;  // sfix33_En22
+  wire signed [32:0] suma2_add_cast1;  // sfix33_En22
+  wire signed [32:0] suma2_add_cast2;  // sfix33_En22
 
-  assign suma2_add_cast = ({In1[35], In1[35:4]}) + $signed({1'b0, In1[3]});
-  assign suma2_add_cast_1 = ({In2[38], In2[38:7]}) + $signed({1'b0, In2[6]});
-  assign suma2_out1 = suma2_add_cast + suma2_add_cast_1;
-
-  assign Out1 = suma2_out1;
+  assign suma2_add_cast1 = {In1[35], In1[35:4]} + In1[3];
+  assign suma2_add_cast2 = {In2[38], In2[38:7]} + In2[6];
+  assign Out1 = suma2_add_cast1 + suma2_add_cast2;
 
 endmodule  // suma2
 
@@ -112,24 +77,13 @@ module suma4
   input   signed [25:0] In2;  // sfix26_En17
   output  signed [41:0] Out1;  // sfix42_En31
 
-  wire signed [41:0] suma4_add_cast;  // sfix42_En31
-  wire signed [42:0] suma4_add_cast_1;  // sfix43_En31
-  wire signed [41:0] suma4_add_cast_2;  // sfix42_En31
-  wire signed [42:0] suma4_add_cast_3;  // sfix43_En31
-  wire signed [42:0] suma4_add_temp;  // sfix43_En31
-  wire signed [41:0] suma4_out1;  // sfix42_En31
+  wire signed [41:0] suma4_add_cast1;  // sfix42_En31
+  wire signed [41:0] suma4_add_cast2;  // sfix42_En31
 
-  assign suma4_add_cast = ((In1[43] == 1'b0) && (In1[42:2] == 41'h1FFFFFFFFFF) ? 42'sh1FFFFFFFFFF :
-              In1[43:2] + $signed({1'b0, In1[1]}));
-  assign suma4_add_cast_1 = {suma4_add_cast[41], suma4_add_cast};
-  assign suma4_add_cast_2 = {{2{In2[25]}}, {In2, 14'b00000000000000}};
-  assign suma4_add_cast_3 = {suma4_add_cast_2[41], suma4_add_cast_2};
-  assign suma4_add_temp = suma4_add_cast_1 + suma4_add_cast_3;
-  assign suma4_out1 = ((suma4_add_temp[42] == 1'b0) && (suma4_add_temp[41] != 1'b0) ? 42'sh1FFFFFFFFFF :
-              ((suma4_add_temp[42] == 1'b1) && (suma4_add_temp[41] != 1'b1) ? 42'sh20000000000 :
-              $signed(suma4_add_temp[41:0])));
+  assign suma4_add_cast1 = In1[43:2] + In1[1];
+  assign suma4_add_cast2 = {{2{In2[25]}}, {In2, 14'b00000000000000}};
 
-  assign Out1 = suma4_out1;
+  assign Out1 = suma4_add_cast1 + suma4_add_cast2;
 
 endmodule  // suma4
 
@@ -142,19 +96,15 @@ module suma6
   input   signed [26:0] In2;  // sfix27_En19
   output  signed [27:0] Out1;  // sfix28_En17
 
-  wire signed [31:0] suma6_add_cast;  // sfix32_En19
-  wire signed [31:0] suma6_add_cast_1;  // sfix32_En19
-  wire signed [31:0] suma6_add_temp;  // sfix32_En19
-  wire signed [27:0] suma6_out1;  // sfix28_En17
+  wire signed [29:0] suma6_add_cast1;  // sfix30_En19
+  wire signed [29:0] suma6_add_cast2;  // sfix30_En19
+  wire signed [29:0] suma6_add_temp;  // sfix30_En19
 
-  assign suma6_add_cast = {{2{In1[28]}}, {In1, 1'b0}};
-  assign suma6_add_cast_1 = {{5{In2[26]}}, In2};
-  assign suma6_add_temp = suma6_add_cast + suma6_add_cast_1;
-  assign suma6_out1 = (((suma6_add_temp[31] == 1'b0) && (suma6_add_temp[30:29] != 2'b00)) || ((suma6_add_temp[31] == 1'b0) && (suma6_add_temp[29:2] == 28'sb0111111111111111111111111111)) ? 28'sb0111111111111111111111111111 :
-              ((suma6_add_temp[31] == 1'b1) && (suma6_add_temp[30:29] != 2'b11) ? 28'sb1000000000000000000000000000 :
-              suma6_add_temp[29:2] + $signed({1'b0, suma6_add_temp[1]})));
+  assign suma6_add_cast1 = {In1, 1'b0};
+  assign suma6_add_cast2 = {{3{In2[26]}}, In2};
+  assign suma6_add_temp = suma6_add_cast1 + suma6_add_cast2;
 
-  assign Out1 = suma6_out1;
+  assign Out1 = suma6_add_temp[29:2] + suma6_add_temp[1];
 
 endmodule  // suma6
 
@@ -167,19 +117,7 @@ module sumb2
   input   signed [20:0] In1;  // sfix21_En13
   output  signed [21:0] Out1;  // sfix22_En14
 
-  wire signed [31:0] sumb2_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb2_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb2_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb2_out1;  // sfix22_En14
-
-  assign sumb2_add_cast = {{10{In1[20]}}, {In1, 1'b0}};
-  assign sumb2_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb2_add_temp = sumb2_add_cast + sumb2_add_cast_1;
-  assign sumb2_out1 = ((sumb2_add_temp[31] == 1'b0) && (sumb2_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb2_add_temp[31] == 1'b1) && (sumb2_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb2_add_temp[21:0])));
-
-  assign Out1 = sumb2_out1;
+  assign Out1 = {In1, 1'b0} + In2;
 
 endmodule  // sumb2_1
 
@@ -192,19 +130,7 @@ module sumb4
   input   signed [20:0] In1;  // sfix21_En13
   output  signed [21:0] Out1;  // sfix22_En14
 
-  wire signed [31:0] sumb4_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb4_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb4_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb4_out1;  // sfix22_En14
-
-  assign sumb4_add_cast = {{10{In1[20]}}, {In1, 1'b0}};
-  assign sumb4_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb4_add_temp = sumb4_add_cast + sumb4_add_cast_1;
-  assign sumb4_out1 = ((sumb4_add_temp[31] == 1'b0) && (sumb4_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb4_add_temp[31] == 1'b1) && (sumb4_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb4_add_temp[21:0])));
-
-  assign Out1 = sumb4_out1;
+  assign Out1 = {In1, 1'b0} + In2;
 
 endmodule  // sumb4_1
 
@@ -217,19 +143,7 @@ module sumb6
   input   signed [19:0] In1;  // sfix20_En12
   output  signed [21:0] Out1;  // sfix22_En14
 
-  wire signed [31:0] sumb6_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb6_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb6_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb6_out1;  // sfix22_En14
-
-  assign sumb6_add_cast = {{10{In1[19]}}, {In1, 2'b00}};
-  assign sumb6_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb6_add_temp = sumb6_add_cast + sumb6_add_cast_1;
-  assign sumb6_out1 = ((sumb6_add_temp[31] == 1'b0) && (sumb6_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb6_add_temp[31] == 1'b1) && (sumb6_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb6_add_temp[21:0])));
-
-  assign Out1 = sumb6_out1;
+  assign Out1 = {In1, 2'b00} + In2;
 
 endmodule  // sumb6_1
 
@@ -242,19 +156,7 @@ module sumb8
   input   signed [20:0] In1;  // sfix21_En13
   output  signed [21:0] Out1;  // sfix22_En14
 
-  wire signed [31:0] sumb8_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb8_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb8_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb8_out1;  // sfix22_En14
-
-  assign sumb8_add_cast = {{10{In1[20]}}, {In1, 1'b0}};
-  assign sumb8_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb8_add_temp = sumb8_add_cast + sumb8_add_cast_1;
-  assign sumb8_out1 = ((sumb8_add_temp[31] == 1'b0) && (sumb8_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb8_add_temp[31] == 1'b1) && (sumb8_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb8_add_temp[21:0])));
-
-  assign Out1 = sumb8_out1;
+  assign Out1 = {In1, 1'b0} + In2;
 
 endmodule  // sumb8_1
 
@@ -267,19 +169,7 @@ module sumb10
   input   signed [20:0] In1;  // sfix21_En13
   output  signed [21:0] Out1;  // sfix22_En14
 
-  wire signed [31:0] sumb10_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb10_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb10_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb10_out1;  // sfix22_En14
-
-  assign sumb10_add_cast = {{10{In1[20]}}, {In1, 1'b0}};
-  assign sumb10_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb10_add_temp = sumb10_add_cast + sumb10_add_cast_1;
-  assign sumb10_out1 = ((sumb10_add_temp[31] == 1'b0) && (sumb10_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb10_add_temp[31] == 1'b1) && (sumb10_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb10_add_temp[21:0])));
-
-  assign Out1 = sumb10_out1;
+  assign Out1 = {In1, 1'b0} + In2;
 
 endmodule  // sumb10_1
 
@@ -291,19 +181,7 @@ module sumb12
   input   signed [21:0] In2;  // sfix22_En14
   input   signed [21:0] In1;  // sfix22_En14
   output  signed [21:0] Out1;  // sfix22_En14
-
-  wire signed [31:0] sumb12_add_cast;  // sfix32_En14
-  wire signed [31:0] sumb12_add_cast_1;  // sfix32_En14
-  wire signed [31:0] sumb12_add_temp;  // sfix32_En14
-  wire signed [21:0] sumb12_out1;  // sfix22_En14
-
-  assign sumb12_add_cast = {{10{In1[21]}}, In1};
-  assign sumb12_add_cast_1 = {{10{In2[21]}}, In2};
-  assign sumb12_add_temp = sumb12_add_cast + sumb12_add_cast_1;
-  assign sumb12_out1 = ((sumb12_add_temp[31] == 1'b0) && (sumb12_add_temp[30:21] != 10'b0000000000) ? 22'sb0111111111111111111111 :
-              ((sumb12_add_temp[31] == 1'b1) && (sumb12_add_temp[30:21] != 10'b1111111111) ? 22'sb1000000000000000000000 :
-              $signed(sumb12_add_temp[21:0])));
-
-  assign Out1 = sumb12_out1;
+  
+  assign Out1 = In1 + In2;
 
 endmodule  // sumb12_1
